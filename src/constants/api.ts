f939c10a235abd9f349caccb3f1d622a1750fa65
atsulevrai <minecraftytom22@gmail.com>
2025-12-21 13:02:35 +0100

    Refactor permission flags to use enums; add API version and gateway opcodes

---
+++ src/constants/api.ts
@@ -0,0 +1,7 @@
+export enum ApiVersion {
+	V6 = 6,
+	V7 = 7,
+	V8 = 8,
+	V9 = 9,
+	V10 = 10,
+}
---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export * from "./api.js";
+export * from "./codes.js";
+export * from "./locales.js";
+export * from "./permissions.js";
